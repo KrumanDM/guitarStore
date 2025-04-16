a81312376c4d76315cac5382cbff6532f935d242
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2025-04-16 18:05:40 +0300

    Создание проекта

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Добро пожаловать в Guitar Store!</h1>
+        </div>
+    );
+};
+
+export default App;
